07da78c2c0a9b70407f7e51bd96725ef2999c586
Sergey Churekov <web-serge@outlook.com>
2023-10-09 21:24:07 +0300

    task#5 edited

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -4,10 +4,10 @@ switch (true) {
     case rule.length > 25:
         console.log( "У меня 100 пудов все получится")
         break
-    case rule.length < 25:
-        console.log("Все таки нет правил без исключения")
+    case rule.length === 25:
+        console.log("50 на 50")
         break
     default:
-        console.log('«50 на 50»')
+        console.log("Все таки нет правил без исключения")
         break
 }
