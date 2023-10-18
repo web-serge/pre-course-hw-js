bbbfd6c84dcc4d295768e603164558bc141da8ab
Sergey Churekov <web-serge@outlook.com>
2023-10-18 20:55:36 +0300

    task#2.3 completed

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,1 +1,8 @@
-// code
+const giveMeRightNumbers = (value1, value2, maximum) => {
+    if (value1 === value2 || value1 <= maximum) {
+        console.log('Success!');
+    } else {
+        console.log('Something wrong!');
+    }
+}
+giveMeRightNumbers(100, 200, 90)
