3dcc1f7d15dfaab14f7592aa0431dba58554132c
Sergey Churekov <web-serge@outlook.com>
2023-10-18 20:51:12 +0300

    task#2.2 completed

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -1,1 +1,9 @@
-// code
+function factorial(n) {
+    debugger
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+console.log(factorial(5));
