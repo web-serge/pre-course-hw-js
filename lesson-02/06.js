4e5e7fd6f67e9788391bf9c29676dff6d7dea0fa
Sergey Churekov <web-serge@outlook.com>
2023-10-18 21:09:00 +0300

    task#2.6 completed

--- lesson-02/06.js
+++ lesson-02/06.js
@@ -1,1 +1,14 @@
-// code
+let passportWithAddress = {
+    name: "Petr",
+    surname: "Petrov",
+    address: {
+        country: "USA",
+        city: "LA"
+    }
+};
+
+let copyPassportWithAddress = {...passportWithAddress, address: {...passportWithAddress.address}}
+copyPassportWithAddress.address.city = 'Bobryisk'
+
+console.log(passportWithAddress.address.city)
+console.log(copyPassportWithAddress.address.city)
