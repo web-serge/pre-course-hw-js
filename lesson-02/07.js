cfa6dd15bcdf33aafa21b88133ac4094be63a3c3
Sergey Churekov <web-serge@outlook.com>
2023-10-18 21:38:06 +0300

    fix 2.7

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -7,7 +7,7 @@ let passportMarried = {
     }
 };
 
-let passportMarried2 = {...passportMarried, married: true,address: {...passportMarried.address}}
-
+let passportMarried2 = {...passportMarried, married: true, address: {...passportMarried.address}}
+passportMarried2.married = true
 console.log(passportMarried)
 console.log(passportMarried2)
